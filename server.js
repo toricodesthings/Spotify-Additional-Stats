5415d16704917c96d310f9fb47d022cf8f8b4690
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-03-18 14:36:39 -0400

    Initial commit, converted code from Python

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { chromium } = require("playwright");
 
 const app = express();
-const PORT = process.env.PORT || 5199;
+const PORT = process.env.PORT || 5000;
 const SPOTIFY_WEB_ENDPOINT = "https://open.spotify.com";
 
 let browserInstance;
